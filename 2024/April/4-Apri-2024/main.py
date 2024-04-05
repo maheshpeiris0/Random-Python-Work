42975200013f157153fb8e7fe21bb0ccbc80b80a
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2024-04-05 00:53:40 +0000

    added

--- 2024/April/4-Apri-2024/main.py
+++ 2024/April/4-Apri-2024/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import os
+import pytz
 
 df=pd.read_json('/workspaces/Random-Python-Work/2024/March/RandomValuesGenerations/stock_by_key/ANVS.json',lines=True)
-
-print(df.head())
+df['end_timestamp'] = pd.to_datetime(df['end_timestamp'], unit='ms',utc=True).dt.tz_convert('US/Eastern')
+print(df['event_type'].count())
+print(df['volume'].sum())
+print(df['end_timestamp'].max())
+print()
+print(df.columns)
