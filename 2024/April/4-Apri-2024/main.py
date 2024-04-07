253dc01f6f8c8f225a19c1083dd77e3e67cbd577
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2024-04-07 14:23:31 +0000

    added

--- 2024/April/4-Apri-2024/main.py
+++ 2024/April/4-Apri-2024/main.py
@@ -6,18 +6,6 @@ df=pd.read_json('/workspaces/Random-Python-Work/2024/March/RandomValuesGeneratio
 df['end_timestamp'] = pd.to_datetime(df['end_timestamp'], unit='ms',utc=True).dt.tz_convert('US/Eastern')
 df.sort_values(by='end_timestamp',ascending=True,inplace=True)
 price_return = (df['close'].iloc[-1] - df['close'].iloc[0])/df['close'].iloc[0]
-dict ={'Return':price_return,'Count':df.shape[0],'Total_Volumne':df['volume'].sum(),'Open':df['open'].min(),'High':df['high'].max()}
+dict ={'Return':price_return,'Close':df['close'].iloc[-1],'Count':df.shape[0],'Total_Volumne':df['volume'].sum(),'AVG_Volume':df['volume'].mean(),'Open':df['open'].min(),'High':df['high'].max()}
 print(dict)
 
-print(df['event_type'].count())
-print(df['volume'].sum())
-print(df['end_timestamp'].max())
-print(df['open'].min())
-
-print(df['close'].max())
-print(df['high'].max())
-
-print(df['close'].max())
-print(df['close'].idxmax())
-print(df['close'].iloc[-1])
-print(df.tail(1))
