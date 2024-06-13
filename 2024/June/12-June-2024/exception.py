216db3b262cda8ee8d29a7e001db227f01ca57a4
Mahesh Peiris <mahesh_sanjaya@yahoo.com>
2024-06-13 00:23:56 +0000

    updated

--- 2024/June/12-June-2024/exception.py
+++ 2024/June/12-June-2024/exception.py
@@ -8,4 +8,23 @@ def number_division(number1, number2):
     else:
         print(result)
         
+number_division(10, 0)
+
+
+def division(x,y):
+    try:
+        result = x/y
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    except TypeError:
+        print("You can't divide by a string!")
+    else:
+        return f"The result is {result}"
+    finally:
+        print("The function has been executed")
         
+print(division(10, 0))
+print(division(10, 2))
+print(division(10, "2"))
+print(division("10", 2))
+print(division("10", "2"))
